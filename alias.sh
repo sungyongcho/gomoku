320ccaa36c663af966744e2ec6a9d2ea07e358d7
Sungyong Cho <dev@sungyongcho.com>
2025-01-12 16:44:33 +0100

    chore: add alias.sh

---
+++ alias.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+### Hypertube
+### Docker compose commands
+
+## for development environment
+alias dev="docker compose -f docker-compose.yml"
+alias dev-up="docker compose -f docker-compose.yml up"
+alias dev-up-build="docker compose -f docker-compose.yml up --build"
+alias dev-down="docker compose -f docker-compose.yml down"
