13a40ba61380e4d885d17f893009bd1835e03715
Woolim Park <woolimi91@naver.com>
2025-01-20 08:59:40 +0900

    feat: how to play page

---
+++ front/components/HowToSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section
+    class="mt-6 rounded-lg border-2 bg-white p-4 text-black [&_b]:font-bold"
+  >
+    <h2 class="text-2xl font-bold">
+      <slot name="title"></slot>
+    </h2>
+
+    <h3 class="text-2xl font-bold">
+      <slot name="subtitle"></slot>
+    </h3>
+
+    <slot name="content"></slot>
+  </section>
+</template>
