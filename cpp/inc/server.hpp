7a9a4066034670af4016a22c12ae747a80bafd6a
Sungyong Cho <dev@sungyongcho.com>
2025-02-03 04:45:30 +0100

    chore: fix directory structures

---
+++ cpp/inc/server.hpp
@@ -0,0 +1,19 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <libwebsockets.h>
+#include <iostream>
+#include <cstring>
+#include "websocket_handler.hpp"
+
+class Server {
+public:
+    Server(int port);
+    void run();
+
+private:
+    struct lws_context *context;
+    struct lws_context_creation_info info;
+};
+
+#endif
---
+++ cpp/inc/websocket_handler.hpp
@@ -0,0 +1,14 @@
+#ifndef WEBSOCKET_HANDLER_HPP
+#define WEBSOCKET_HANDLER_HPP
+
+#include <libwebsockets.h>
+#include <iostream>
+#include <string>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
+int callback_debug(struct lws *wsi, enum lws_callback_reasons reason,
+                   void *user, void *in, size_t len);
+
+#endif
