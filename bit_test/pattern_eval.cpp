b022517c9e501dd0c7e2454ed22c8f7b078879b8
Sungyong Cho <dev@sungyongcho.com>
2025-04-06 01:48:44 +0200

    wip: about to change evaluation code

--- bit_test/pattern_eval.cpp
+++ bit_test/pattern_eval.cpp
@@ -37,8 +37,8 @@ static const int blockScores[6] = {
 // For each window position, it prints the full binary string with the current 2-bit window in
 // brackets, then prints the window's bits (and "matches" if it equals 'player'). If 'reverse' is
 // true, the sliding is done in reverse order.
-void slideWindow(int side, int player, bool reverse, int &continuous, bool &isClosedEnd,
-                 int &continuousEmpty) {
+void slideWindowContinuous(int side, int player, bool reverse, int &continuous, bool &isClosedEnd,
+                           int &continuousEmpty) {
   int opponent = player == 1 ? 2 : 1;
   (void)reverse;
   int player_count = 0;
@@ -48,7 +48,6 @@ void slideWindow(int side, int player, bool reverse, int &continuous, bool &isCl
   if (!reverse) {
     for (int i = 0; i < SIDE_WINDOW_SIZE; ++i) {
       int target_bit = ((side >> ((SIDE_WINDOW_SIZE - i - 1) * 2)) & 0x03);
-      std::cout << target_bit << std::endl;
       if (target_bit == player) {
         if (continuous == i) continuous++;
         if (player_begin == 0) player_begin = i + 1;
@@ -66,7 +65,6 @@ void slideWindow(int side, int player, bool reverse, int &continuous, bool &isCl
   } else {
     for (int i = 0; i < SIDE_WINDOW_SIZE; ++i) {
       int target_bit = ((side >> (i * 2)) & 0x03);
-      std::cout << target_bit << std::endl;
       if (target_bit == player) {
         if (continuous == i) continuous++;
         if (player_begin == 0) player_begin = i + 1;
@@ -90,26 +88,142 @@ void slideWindow(int side, int player, bool reverse, int &continuous, bool &isCl
   // std::cout << "continuousEmpty: " << continuousEmpty << std::endl;
   // std::cout << "----------------" << std::endl;
 
-  // if (player_count == continuous) {
-  //   std::cout << "continuous in: " << continuous << std::endl;
-  //   if (closed - continuous == 1) {
-  //     std::cout << "but closed" << std::endl;
-  //     isClosedEnd = true;
-  //   }
-  // }
+  if (player_count == continuous) {
+    if (closed - continuous == 1) {
+      isClosedEnd = true;
+    }
+  }
   std::cout << "----------------" << std::endl;
 }
 
-bool isCaptureThreatning(int side, int player) {
+void slideWindowBlock(int side, int player, bool reverse, int &blockContinuous, bool &isClosedEnd) {
+  int opponent = player == 1 ? 2 : 1;
+  int blockContinuousEmpty = 0;
+  int block_begin = 0;
+  int closed = 0;
+
+  if (!reverse) {
+    for (int i = 0; i < SIDE_WINDOW_SIZE; ++i) {
+      int target_bit = ((side >> ((SIDE_WINDOW_SIZE - i - 1) * 2)) & 0x03);
+      if (target_bit == opponent) {
+        if (blockContinuous == i) blockContinuous++;
+      } else if (target_bit == player || target_bit == OUT_OF_BOUNDS) {
+        if (closed == 0) closed = i + 1;
+      }
+    }
+    for (int i = SIDE_WINDOW_SIZE - blockContinuous; i > 0; i--) {
+      if (((side >> ((i - 1) * 2)) & 0x03) == EMPTY_SPACE)
+        blockContinuousEmpty += 1;
+      else
+        break;
+    }
+  } else {
+    for (int i = 0; i < SIDE_WINDOW_SIZE; ++i) {
+      int target_bit = ((side >> (i * 2)) & 0x03);
+      if (target_bit == opponent) {
+        if (blockContinuous == i) blockContinuous++;
+      } else if (target_bit == player || target_bit == OUT_OF_BOUNDS) {
+        if (closed == 0) closed = i + 1;
+      }
+    }
+    for (int i = blockContinuous; i < SIDE_WINDOW_SIZE; i++) {
+      if (((side >> (i * 2)) & 0x03) == EMPTY_SPACE)
+        blockContinuousEmpty += 1;
+      else
+        break;
+    }
+  }
+
+  if (closed - blockContinuous == 1) {
+    isClosedEnd = true;
+  }
+}
+
+// capture vulnerable (for player 1)
+// forward
+// 1. .?12
+// 2. .1?2
+// backward
+// 3. 21?.
+// 4. 2?1.
+
+bool isCaptureVulnerable(int forward, int backward, int player) {
   int opponent = player == 1 ? 2 : 1;
 
-  if (((side & 0xFC) >> 2) == pack_cells_3(player, player, opponent)) return true;
+  if (((forward & 0xF0) >> 4) == pack_cells_2(player, opponent) && (backward & 0x03 == EMPTY_SPACE))
+    return true;
+  if (((forward & 0xC0) >> 6) == opponent && (backward & 0x0F) == pack_cells_2(EMPTY_SPACE, player))
+    return true;
+
+  if (((backward & 0x0F) == pack_cells_2(opponent, player)) &&
+      (((forward & 0xC0) >> 6) == EMPTY_SPACE))
+    return true;
+
+  if (((backward & 0x03) == opponent) &&
+      (((forward & 0x0F) >> 4) == pack_cells_2(player, EMPTY_SPACE)))
+    return true;
+
   return false;
 }
 
+bool isCaptureWarning(int side, int player, bool reverse) {
+  int opponent = player == 1 ? 2 : 1;
+
+  // check forward
+  if (!reverse) {
+    if (((side & 0xFC) >> 2) == pack_cells_3(player, player, opponent)) return true;
+  } else {
+    if ((side & 0x3F) == pack_cells_3(opponent, player, player)) return true;
+  }
+  return false;
+}
+
+void printAxis(int forward, int backward) {
+  // Process backward 8 bits in 2-bit groups (from MSB to LSB)
+  for (int i = 3; i >= 0; i--) {
+    int val = (backward >> (i * 2)) & 0x03;
+    switch (val) {
+      case 0:
+        std::cout << ".";
+        break;
+      case 1:
+        std::cout << "1";
+        break;
+      case 2:
+        std::cout << "2";
+        break;
+      case 3:
+        std::cout << "X";
+        break;
+    }
+  }
+  // Print the middle marker "[.]"
+  std::cout << "[.]";
+
+  // Process forward 8 bits in 2-bit groups (from MSB to LSB)
+  for (int i = 3; i >= 0; i--) {
+    int val = (forward >> (i * 2)) & 0x03;
+    switch (val) {
+      case 0:
+        std::cout << ".";
+        break;
+      case 1:
+        std::cout << "1";
+        break;
+      case 2:
+        std::cout << "2";
+        break;
+      case 3:
+        std::cout << "X";
+        break;
+    }
+  }
+  std::cout << std::endl;
+}
+
 int main() {
-  int forward = 0b01011000;
-  int backward = 0b00100101;
+  int forward = 0b10010010;
+  int backward = 0b00011010;
   int player = 1;
 
   int forwardContinuous = 0;
@@ -119,10 +233,15 @@ int main() {
   int backwardContinuous = 0;
   bool backwardClosedEnd = false;
   int backwardContinuousEmpty = 0;
+
+  int score = 0;
+
+  printAxis(forward, backward);
   // Slide window in forward direction.
-  slideWindow(forward, player, false, forwardContinuous, forwardClosedEnd, forwardContinuousEmpty);
-  slideWindow(backward, player, true, backwardContinuous, backwardClosedEnd,
-              backwardContinuousEmpty);
+  slideWindowContinuous(forward, player, false, forwardContinuous, forwardClosedEnd,
+                        forwardContinuousEmpty);
+  slideWindowContinuous(backward, player, true, backwardContinuous, backwardClosedEnd,
+                        backwardContinuousEmpty);
 
   std::cout << "----------------" << std::endl;
   std::cout << "forward continuous: " << forwardContinuous << std::endl;
@@ -139,13 +258,72 @@ int main() {
   int totalContinuous = forwardContinuous + backwardContinuous;
 
   // 1.if continous on both side, it'll be gomoku (5 in a row)
-  if (totalContinuous >= 4) totalContinuous = 5;
+  if (totalContinuous >= 4) totalContinuous = 4;
 
   if (totalContinuous < 4) {
     // 2. for condition where total continous are less than equal to
     // if both ends are closed, it is meaningless to place the stone.
     if (backwardClosedEnd == true && forwardClosedEnd == true) totalContinuous = 0;
+
+    // 3. if the total continuous + continuous empty => potential growth for gomoku is less then
+    // five, don't need to extend the line
+    else if ((totalContinuous + forwardContinuousEmpty < 5) ||
+             (totalContinuous + backwardContinuousEmpty < 5) ||
+             (totalContinuous + backwardContinuousEmpty + forwardContinuousEmpty < 5))
+      totalContinuous = 0;
+
+    // 4. prevent from opponent to capture (needs to check if necessary)
+    // separated if condition because it needs to check all above then add
+    if (isCaptureWarning(forward, player, false) || isCaptureWarning(backward, player, true))
+      totalContinuous = forwardContinuous + backwardContinuous;
   }
+
+  score += continuousScores[totalContinuous + 1];
+
+  std::cout << "continuous: " << totalContinuous << " score: " << score << std::endl;
+  std::cout << "=====================================" << std::endl;
+
+  int forwardBlockContinuous = 0;
+  bool forwardBlockClosedEnd = false;
+
+  int backwardBlockContinuous = 0;
+  bool backwardBlockClosedEnd = false;
+  slideWindowBlock(forward, player, false, forwardBlockContinuous, forwardBlockClosedEnd);
+  slideWindowBlock(backward, player, true, backwardBlockContinuous, backwardBlockClosedEnd);
+
+  std::cout << "----------------" << std::endl;
+  std::cout << "forward block continuous: " << forwardBlockContinuous << std::endl;
+  std::cout << "forward block is closed end: " << forwardBlockClosedEnd << std::endl;
+  std::cout << "----------------" << std::endl;
+
+  std::cout << "----------------" << std::endl;
+  std::cout << "backward continuous: " << backwardBlockContinuous << std::endl;
+  std::cout << "backward block is closed end: " << backwardBlockClosedEnd << std::endl;
+  std::cout << "----------------" << std::endl;
+  std::cout << false << std::endl;
+
+  int totalBlockCont = forwardBlockContinuous + backwardBlockContinuous;
+  // 1.if continous opponent is bigger or equal, should block asap
+  if (totalBlockCont >= 4) totalBlockCont = 4;
+
+  if (totalBlockCont < 4) {
+    // 2. if both end is blocked by player and continous is less then three, there is no need to
+    // block
+    if (forwardBlockClosedEnd && backwardBlockClosedEnd) totalBlockCont = 0;
+    // 3. for each side, if one side continous but that side is already closed,
+    // it doesn't need to be blocked 'yet', so heuristics can go for better score moves.
+    else if (forwardBlockClosedEnd && (forwardBlockContinuous == totalBlockCont) ||
+             backwardBlockClosedEnd && (backwardBlockContinuous == totalBlockCont)) {
+      totalBlockCont = 0;
+      // 3-2. but if it can be captured, add up the score (check)
+      if (forwardBlockContinuous == 2) totalBlockCont += 1;
+      if (backwardBlockContinuous == 2) totalBlockCont += 1;
+    }
+  }
+
+  std::cout << "block: " << totalBlockCont << " score: " << blockScores[totalBlockCont + 1]
+            << std::endl;
+  std::cout << "=====================================" << std::endl;
 
   return 0;
 }
