86478c8841add17d455af4d699baaf3f51eb1503
Woolim Park <woolimi91@naver.com>
2025-04-22 00:14:27 +0200

    delete log

--- cpp/src/gomoku/EvaluationHard.cpp
+++ cpp/src/gomoku/EvaluationHard.cpp
@@ -147,15 +147,6 @@ EvaluationEntry evaluateContinuousPatternHard(unsigned int backward, unsigned in
     }
   }
 
-  if (backward == 0b00000000 && forward == 0b00000000) {
-    int fw = 0b11111111;
-    int bw = 0b00011010;
-    std::cout << "IsValidForward" << isValidForwardPattern(fw) << std::endl;
-    std::cout << "IsValidBackward" << isValidBackwardPattern(bw) << std::endl;
-
-    printAxis(fw, bw);
-  }
-
   if (isCaptureVulnerable(forward, backward, player)) {
     returnValue.counts.captureVulnerable += 1;
   }
