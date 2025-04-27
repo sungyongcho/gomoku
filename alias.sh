ec0deb2e4d210aa7a095a24b92935d1014260a1c
Sungyong Cho <dev@sungyongcho.com>
2025-04-27 20:45:19 +0200

    feat: vscode debug

--- alias.sh
+++ alias.sh
@@ -7,6 +7,7 @@
 alias dev="docker compose -f docker-compose.yml"
 alias dev-up="docker compose -f docker-compose.yml up back front cpp_server"
 alias dev-up-valgrind="docker compose -f docker-compose.yml up back front cpp_server_valgrind"
+alias dev-up-debug="docker compose -f docker-compose.yml up back front cpp_server_debug"
 # alias dev-up-valgrind-build="docker compose -f docker-compose.yml up back front cpp_server_valgrind"
 # alias dev-up-build="docker compose -f docker-compose.yml up back front cpp_server --build"
 alias dev-down="docker compose -f docker-compose.yml down"
