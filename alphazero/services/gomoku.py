30a22e4961a778c75c4401cb30b0d2dd6bf8adca
Sungyong Cho <dev@sungyongcho.com>
2025-05-24 15:23:36 +0200

    wip: python/alphazero re-setup

--- alphazero/services/gomoku.py
+++ alphazero/services/gomoku.py
@@ -1,7 +1,6 @@
 import time
 from typing import List
 
-from ai.minmax import minmax
 from constants import EMPTY_SPACE
 from rules.capture import capture_opponent
 from rules.doublethree import check_doublethree
