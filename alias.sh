280c69381be72361b7f28372fd2e47f3c0d579dc
Sungyong Cho <dev@sungyongcho.com>
2025-05-24 15:23:36 +0200

    wip: python/alphazero re-setup

--- alias.sh
+++ alias.sh
@@ -5,8 +5,8 @@
 
 ## for development environment
 alias dev="docker compose -f docker-compose.yml"
-alias dev-up="docker compose -f docker-compose.yml up back front minimax"
-alias dev-up-valgrind="docker compose -f docker-compose.yml up back front minimax_valgrind"
-alias dev-up-debug="docker compose -f docker-compose.yml up back front minimax_debug"
+alias dev-up="docker compose -f docker-compose.yml up front minimax alphazero"
+alias dev-up-valgrind="docker compose -f docker-compose.yml up front minimax_valgrind"
+alias dev-up-debug="docker compose -f docker-compose.yml up front minimax_debug"
 alias dev-down="docker compose -f docker-compose.yml down"
 
