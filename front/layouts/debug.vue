4e42e260665a628324d8f68c1747a34f129a5e2a
Woolim Park <woolimi91@naver.com>
2025-05-25 15:32:09 +0200

    UI done

--- front/layouts/debug.vue
+++ front/layouts/debug.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <MHeader debug />
+  <div class="h-screen">
+    <MHeader debug class="fixed left-0 top-0 z-10 w-full" />
     <slot></slot>
-    <MFooter />
+    <MFooter class="fixed bottom-0 left-0 z-10 w-full" />
   </div>
 </template>
