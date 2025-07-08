1437129bf2595726bd4b42e79d91eab464eecacd
Sungyong Cho <dev@sungyongcho.com>
2025-07-08 13:42:32 +0200

    chore: epsilon correction in selfPlay

--- alphazero/gmk/alphazero.py
+++ alphazero/gmk/alphazero.py
@@ -19,40 +19,51 @@ class AlphaZero:
         self.mcts = PVMCTS(game, args, model)
 
     def selfPlay(self):
-        memory = []
-        state: GameState = self.game.get_initial_state()
-        player = state.next_player
-        while True:
-            action_probs = self.mcts.search(state)
-            memory.append((state, action_probs, player))
-
-            temperature_action_probs = action_probs ** (1 / self.args["temperature"])
-            temperature_action_probs /= temperature_action_probs.sum()  # ← 정규화 추가
-            flat_idx = np.random.choice(
-                self.game.action_size, p=temperature_action_probs
-            )
-
-            # 평탄 인덱스 → 2D 좌표
-            x = flat_idx % self.game.col_count  # 열
-            y = flat_idx // self.game.col_count  # 행
-            action = (x, y)
-
-            state = self.game.get_next_state(state, action, player)
-
-            value, is_terminal = self.game.get_value_and_terminated(state, action)
-
-            if is_terminal:
-                returnMemory = []
-                for hist_state, hist_action_probs, hist_player in memory:
-                    hist_outcome = value if hist_player == player else -value
-                    returnMemory.append((hist_state, hist_action_probs, hist_outcome))
-                return returnMemory
+        self.model.eval()
+        with torch.no_grad():
+            memory = []
+            state: GameState = self.game.get_initial_state()
             player = state.next_player
+
+            while True:
+                action_probs = self.mcts.search(state)
+                memory.append((state, action_probs, player))
+
+                temperature_action_probs = np.maximum(action_probs, 1e-8)  # ε 보정
+                temperature_action_probs = action_probs ** (
+                    1 / self.args["temperature"]
+                )
+                temperature_action_probs /= (
+                    temperature_action_probs.sum()
+                )  # ← 정규화 추가
+                flat_idx = np.random.choice(
+                    self.game.action_size, p=temperature_action_probs
+                )
+
+                # 평탄 인덱스 → 2D 좌표
+                x = flat_idx % self.game.col_count  # 열
+                y = flat_idx // self.game.col_count  # 행
+                action = (x, y)
+
+                state = self.game.get_next_state(state, action, player)
+
+                value, is_terminal = self.game.get_value_and_terminated(state, action)
+
+                if is_terminal:
+                    returnMemory = []
+                    for hist_state, hist_action_probs, hist_player in memory:
+                        hist_outcome = value if hist_player == player else -value
+                        returnMemory.append(
+                            (hist_state, hist_action_probs, hist_outcome)
+                        )
+                    return returnMemory
+                player = state.next_player
 
     def train(self, memory):
         random.shuffle(memory)
         bsz = self.args["batch_size"]
 
+        self.model.train()
         for start in range(0, len(memory), bsz):
             sample = memory[start : start + bsz]
 
@@ -74,7 +85,7 @@ class AlphaZero:
             out_policy, out_value = self.model(state)
 
             # ① 확률 분포 그대로 사용
-            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            policy_loss = F.cross_entropy(out_policy, policy_targets, reduction="mean")
             value_loss = F.mse_loss(out_value, value_targets)
             # print(policy_loss.item(), value_loss.item())
             loss = policy_loss + value_loss
