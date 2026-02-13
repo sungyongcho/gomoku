3fdc84ca6469297f75eecb1ecae868ff84f623e1
Woolim Park <woolimi91@naver.com>
2026-02-14 07:38:55 +0900

    feat: markdown with katex

--- front/layouts/docs.vue
+++ front/layouts/docs.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css",
+    },
+  ],
+});
+</script>
+
 <template>
   <div class="h-screen">
     <MHeader doc-page class="w-full" />
